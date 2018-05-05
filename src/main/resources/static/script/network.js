a256eee4ee9521a9182a06a359ddca1410aaf93a
Frapples <isfrapples@outlook.com>
2018-05-05 17:02:01 +0800

    Add vue

---
+++ src/main/resources/static/script/network.js
@@ -0,0 +1,73 @@
+"use strict";
+
+(function () {
+    var apis = [
+        {
+            name: 'demoAPI',
+            url: '/api/queryDemo',
+            method: "POST",
+        },
+    ];
+
+    var Network = function () {
+        var self = {};
+        self.apis = apis;
+
+        var converter = function (response) {
+            return new Promise(function(resolve, reject) {
+                if (response.status == 200 && response.data.code == 200) {
+                    resolve(response.data.data);
+                } else {
+                    (new Vue).$notify.error({
+                        title: 'Error',
+                        message: 'System error, please contact the administrator'
+                    });
+                    reject();
+                }
+            });
+        };
+
+        var onError = function (error) {
+            (new Vue).$notify.error({
+                title: 'Error',
+                message: 'Network error'
+            });
+        };
+
+        self.post = function (url, data) {
+            var params = new URLSearchParams();
+            for (var key in data) {
+                params.append(key, data[key]);
+            }
+
+            return axios.post(url, params).then(converter).catch(onError);
+        };
+
+        self.get = function (url, data) {
+            return axios.get(url, {
+                params: data,
+            }).then(converter).catch(onError);
+        };
+
+        self.initApi = function () {
+            self.api = {};
+            self.apis.forEach(function (api) {
+                self.api[api.name] = function (data) {
+                    if (api.method == "POST") {
+                        return self.post(api.url, data);
+                    } else if (api.method == "GET") {
+                        return self.post(api.url, data);
+                    } else {
+                        console.error("不知道对请求类型：" + api.method);
+                    }
+                };
+            });
+        };
+
+        return self;
+    };
+
+    window.network = Network();
+    window.network.initApi();
+
+})();
---
+++ src/main/resources/static/script/util.js
@@ -0,0 +1,95 @@
+'use strict';
+
+function getQueryString(name)
+{
+    var vars = window.location.search.substring(1).split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == name) {
+            return pair[1];
+        }
+    }
+    return null;
+}
+
+function getFormParas(selector) {
+    var paras = $(selector).serializeArray();
+
+    var result = {};
+    paras.forEach(function(item) {
+        result[item.name] = item.value;
+    });
+    return result;
+}
+
+function gotoUrl(url) {
+    window.location.href = url;
+}
+
+function numberRound(num, bit) {
+    var n = Math.pow(10, bit);
+    return Math.round(num * n) / n;
+}
+
+function sprintf(format) {
+    var i = 1;
+    var items = arguments;
+    return format.replace(/\%([0-9]*)[ds]/g, function (substring, num) {
+        var item = String(items[i]);
+        if (num.length > 0) {
+            var length = Number(num);
+            if (item.length < length) {
+                var remain = length - item.length;
+                while (remain--) {
+                    var prefix = num[0] === "0" ? "0" : " ";
+                    item = prefix + item;
+                }
+            }
+        }
+        i++;
+        return item;
+    });
+}
+
+function formatDateTime(timestamp) {
+    timestamp = Number(timestamp);
+    var date = new Date(timestamp);
+    return sprintf("%s-%02s-%02s %02s:%02s:%02s",
+        date.getFullYear(),
+        date.getMonth() + 1,
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds());
+}
+
+function formatDate(timestamp) {
+    timestamp = Number(timestamp);
+    var date = new Date(timestamp);
+    return sprintf("%s-%02s-%02s",
+        date.getFullYear(),
+        date.getMonth() + 1,
+        date.getDate());
+}
+
+function getExploreName(){
+// https://segmentfault.com/a/1190000007640795
+    var userAgent = navigator.userAgent;
+    if(userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1){
+        return 'Opera';
+    } else if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1){
+        return 'IE';
+    } else if (userAgent.indexOf("Edge") > -1){
+        return 'Edge';
+    } else if (userAgent.indexOf("Firefox") > -1){
+        return 'Firefox';
+    } else if (userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1){
+        return 'Safari';
+    } else if (userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1){
+        return 'Chrome';
+    } else if (!!window.ActiveXObject || "ActiveXObject" in window){
+        return 'IE>=11';
+    } else {
+        return 'Unkonwn';
+    }
+}
